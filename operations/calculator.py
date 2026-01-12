d4ddba5fb2d78bd719dc0d61a7882628d22c3f5b
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-12 11:12:30 -0600

    Lint added

--- operations/calculator.py
+++ operations/calculator.py
@@ -1,12 +1,17 @@
+def add(x, y):
+    return x + y
 
-def add(x,y):
-    return x+y
-def sub(x,y):
-    return x-y
-def mul(x,y):
-    return x*y
-def div(x,y):
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
     if y == 0:
         print("denominator can't be zero")
     else:
-        return x/y
+        return x / y
